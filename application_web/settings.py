ff3032372c73bc457a13e72981671aba4a3d497c
WALID AZHARI <walidazhari519@gmail.com>
2022-07-13 19:38:58 +0100

    Solving the problems !!!

--- application_web/settings.py
+++ application_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-61k5qr(^^_*0q4zi)aoy(m=-=g21#ym53mt0&9g=zy(+f3c&v='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'casa-city.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
