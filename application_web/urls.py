014ee018bb4d37489a1ad81bc4f92006d9bdbc03
WALID AZHARI <walidazhari519@gmail.com>
2022-07-15 12:04:42 +0100

    Updating urls & making migrates !!!

--- application_web/urls.py
+++ application_web/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('WWTP', views.wwtp, name='WWTP'),
     path('School', views.school, name='School'),
-    path('Tourist', views.tourist, name='Tourist'),
+    path('Touristic', views.tourist, name='Tourist'),
 
 ]
