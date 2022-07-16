6928164f44d6102f0835a1299b85a9e1d044846c
WALID AZHARI <walidazhari519@gmail.com>
2022-07-16 01:47:20 +0100

    !!!

--- application_web/settings.py
+++ application_web/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/home/WalidAz/myproject/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app_django/static')
